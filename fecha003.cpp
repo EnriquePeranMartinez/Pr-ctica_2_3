6f28831b956a32c04cc4a0feaecddf4b60e80a1f
Enrique Peran <enrique.peranm@um.es>
2025-11-01 18:10:36 +0100

    fecha003 medio hecho

--- fecha003.cpp
+++ fecha003.cpp
@@ -55,7 +55,7 @@ void frase_a_numero(string frase){
     }
 }
 
-int dias_meses[13] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }
+int dias_meses[13] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
 
 class Fecha {
@@ -64,32 +64,87 @@ class Fecha {
 		int hour, minute, second;
 	public:
 		Fecha();
-		bool leer();
+		Fecha leer();
 		void escribir();
 		bool es_menor(Fecha &otra);
 		bool es_igual(Fecha &otra);
 };
 
 
-Fecha::Fecha (){ }
-bool Fecha::leer()
-{ ... }
+Fecha::Fecha (){ }	// Constructor
+
+Fecha Fecha::leer()
+{ 	
+	// Leer Días
+	char d[2];
+	d[0] = cin.get();
+	d[1] = cin.get();
+
+	if (d[1] != '/')
+	{
+		day = atoi(d);		// Como es un char*, podemos usar atoi
+		cin.ignore(); // Ignoramos la barra
+	}
+	else
+	{
+		day = d[0] - '0'; // Restamos 48 al código ascii del carácter para que nos dé el número entero 
+	}
+
+	// Leer meses
+	char m[2];
+	m[0] = cin.get();
+	m[1] = cin.get();
+
+	if (m[1] != '/')
+	{
+		month = atoi(m);		// Como es un char*, podemos usar atoi
+		cin.ignore(); // Ignoramos la barra
+	}
+	else
+	{
+		month = m[0] - '0'; // Restamos 48 al código ascii del carácter para que nos dé el número entero 
+	}
+
+	// Leer año
+	cin >> year; // Leemos hasta el espacio en blanco
+
+	// Leer horas
+	char h[2];
+	h[0] = cin.get();
+	h[1] = cin.get();
+	hour = atoi(h);
+	cin.ignore();
+	// Leer minutos
+	char mi[2];
+	mi[0] = cin.get();
+	mi[1] = cin.get();
+	minute = atoi(mi);
+	cin.ignore();
+	// Leer minutos
+	char s[2];
+	s[0] = cin.get();
+	s[1] = cin.get();
+	second = atoi(s);
+	cin.ignore();
+
+}
 void Fecha::escribir()
-{ ... }
-bool Fecha::es_menor(Fecha &otra);
-{ ... }
-bool Fecha::es_igual(Fecha &otra);
-{ ... }
+{ /*...*/ }
+bool Fecha::es_menor(Fecha &otra)
+{ return false; }
+bool Fecha::es_igual(Fecha &otra)
+{ /*...*/ }
 
 int main(){
     int N;
-    Fecha, factual, fanterior;
+    Fecha factual, fanterior;
 
     cin >> N;
     cin.ignore();
 
     for (int i = 0; i < N; i++)
     {   
+		factual.leer();
         
     }
 
